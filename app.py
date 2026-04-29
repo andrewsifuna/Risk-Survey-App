8599745a027a8817f20a74a39d6f2225e43cfd5c
Andy Mukite <38553010+andrewsifuna@users.noreply.github.com>
2026-04-29 04:54:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2251,18 +2251,14 @@ elif section == "Submit":
         story.append(Spacer(1, SUBSECTION_SPACING))
 
         # TABLE HEADER + EMPTY ROWS
-        recommendation_table_data = [
-            [
-                Paragraph("<b>Recommendation</b>", normal),
-                Paragraph("<b>Description of Action</b>", normal),
-                Paragraph("<b>Cost Type</b>", normal),
-                Paragraph("<b>Implementation<br/>Timeline</b>", normal),
-            ],
+        empty_row = [Paragraph("", normal) for _ in range(4)]
 
-            [Paragraph("", normal), Paragraph("", normal), Paragraph("", normal), Paragraph("", normal)],
-            [Paragraph("", normal), Paragraph("", normal), Paragraph("", normal), Paragraph("", normal)],
-            [Paragraph("", normal), Paragraph("", normal), Paragraph("", normal), Paragraph("", normal)],
-            [Paragraph("", normal), Paragraph("", normal), Paragraph("", normal), Paragraph("", normal)],
+        recommendation_table_data = [
+            header_row,
+            empty_row,
+            empty_row,
+            empty_row,
+            empty_row,
         ],
 
         available_width = doc.width
